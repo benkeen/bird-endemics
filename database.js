7c73f49a1aa2833f7ce1f0b377cd0efee603c997
Ben Keen <ben.keen@gmail.com>
2017-05-12 23:04:01 -0700

    sync

--- database.js
+++ database.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql');
-var connection = mysql.createConnection({
+
+// connect to the database
+const connection = mysql.createPool({
   host     : 'localhost',
   user     : 'root',
   password : 'root',
@@ -31,12 +33,97 @@ const addCountry = function (country_code, country_name) {
   });
 };
 
-const clearCommonSightings = function (country_id) {
 
+const addNewCountrySpecies = function (countrySpeciesId, parts) {
+  const countryCode = parts[11];
+  const commonName = parts[3];
+  const sciName = parts[4];
+
+  connection.query({
+    sql: `
+      INSERT INTO country_species
+      SET country_species_id = ?,
+          country_code = ?,
+          common_name = ?,
+          sci_name = ?,
+          num_obs = 0,
+          over_threshold = 'no'
+    `,
+    values: [countrySpeciesId, countryCode, commonName, sciName]
+  }, function (err, result) {
+    if (err) {
+      console.log('error adding to country_species (', countryCode, commonName, sciName, ')');
+      return;
+    }
+    // now add the first record for this country
+    addSighting(countrySpeciesId, parts);
+  });
+};
+
+
+const addSighting = function(countrySpeciesId, parts) {
+//  connection.query({
+//    sql: 'UPDATE country_species SET num_obs = num_obs + 1 WHERE country_species_id = ?',
+//    values: [countrySpeciesId]
+//  }, function (err1, results) {
+//    if (err1) {
+//      return;
+//    }
+    connection.query({
+      sql: 'INSERT INTO rare_sightings (country_species_id, row_data) VALUES (?, ?)',
+      values: [countrySpeciesId, parts.join('\t')]
+    }, function (err2, results) {
+    });
+//  });
+};
+
+
+const markCountrySpeciesAsComplete = function (countrySpeciesId) {
+  connection.query({
+    sql: 'UPDATE country_species SET over_threshold = "yes" WHERE id = ?',
+    values: [countrySpeciesId]
+  }, function (error, results) {
+    if (error) {
+      console.log('error setting OVER', error);
+    }
+    connection.query({
+      sql: 'DELETE FROM rare_sightings WHERE id = ?',
+      values: [countrySpeciesId]
+    }, function (err2, results) {
+      console.log(`Threshold met for ${countrySpeciesId}. Cleared rare_sightings.`);
+    });
+  });
+};
+
+
+const cache = {};
+let id = 1;
+
+const processSighting = function (parts, maxThreshold) {
+  let countryCode = parts[11];
+  let sciName = parts[4];
+
+  let key = countryCode + sciName;
+  if (!cache.hasOwnProperty(key)) {
+    const newKey = id++;
+    cache[key] = [1, newKey, false]; // count, over-threshold
+    addNewCountrySpecies(newKey, parts);
+  } else if (!cache[key][2]) {
+    cache[key][0]++;
+    if (cache[key][0] > maxThreshold) {
+      markCountrySpeciesAsComplete(cache[key][1]);
+      cache[key][2] = true;
+    } else {
+      addSighting(cache[key][1], parts);
+    }
+  }
 };
 
 
 module.exports = {
   addCountry: addCountry,
-  clearCommonSightings: clearCommonSightings
+  addNewCountrySpecies: addNewCountrySpecies,
+  addSighting: addSighting,
+  markCountrySpeciesAsComplete: markCountrySpeciesAsComplete,
+  processSighting
 };
--- find-endemics.js
+++ find-endemics.js
@@ -10,7 +10,7 @@ const util = require('util');
 const es = require('event-stream');
 const db = require('./database');
 
-const SOURCE_FILES = ["./files/file3.txt"];
+const SOURCE_FILES = ["./files/file5.txt"];
 const RARITY_MAX_OBS_THRESHOLD = 20;
 
 
@@ -19,39 +19,43 @@ const parseFile = function (filename) {
   let isRowOne = true;
   let counter = 1;
 
-  fs.createReadStream(SOURCE_FILES[0])
+  fs.createReadStream(filename)
     .pipe(es.split())
     .pipe(es.mapSync(function (line) {
       if (isRowOne) {
         isRowOne = false;
         return;
       }
-
+//      if (counter > 10000) {
+//        return;
+//      }
       processRow(line);
 
-      if (counter % 100000 === 0) {
-        console.log('- row ' + counter);
+      if (counter % 500000 === 0) {
+        console.log('- lines processed: ' + counter);
       }
       counter++;
     }))
     .on('end', function() {
-       console.log("The file was saved.");
+       console.log("The file was processed.");
     });
-
 };
 
 
 const processRow = function (line) {
   const parts = line.split(/\t/);
-  const country_name = parts[10];
-  const country_code = parts[11];
+//  const country_name = parts[10];
+//  const country_code = parts[11];
+
+  if (parts.length < 30) {
+    console.log('[invalid row data]');
+  }
 
   // for each file, run the code twice, first running maybeAddCountry() with processSighting commented out, then the
   // opposite. (don't have to do this!)
-  maybeAddCountry(country_name, country_code);
-  processSighting(parts);
+  //maybeAddCountry(country_name, country_code);
+  db.processSighting(parts, RARITY_MAX_OBS_THRESHOLD);
 };
-
 
 
 // safely adds a country to the DB. This is a wrapper that memoizes the countries added in this session (i.e. this
@@ -66,59 +70,6 @@ const maybeAddCountry = function (country_name, country_code) {
 };
 
 
-const processSighting = function () {
-
-};
-
 parseFile(SOURCE_FILES[0]);
-
-
-//    let parts = line.split('\t');
-//
-//    let countryCode = parts[11];
-//    if (!data.hasOwnProperty(countryCode)) {
-//      data[countryCode] = {};
-//    }
-//    let commonName = parts[3];
-//    if (!data[countryCode].hasOwnProperty(commonName)) {
-//      data[countryCode][commonName] = {
-//        obs: [],
-//        numObs: 0, // for speed
-//        isRare: true // once RARITY_MAX_OBS_THRESHOLD is reached, this species is no longer considered rare in this country
-//      };
-//    }
-//
-//    counter++;
-//
-//    // if we've seen a zillion of these species already, ignore the data
-//    if (!data[countryCode][commonName].isRare) {
-//      return;
-//    }
-//
-//    // clean house.
-//    if (data[countryCode][commonName].numObs >= RARITY_MAX_OBS_THRESHOLD) {
-//      data[countryCode][commonName] = {
-//        obs: [],
-//        isRare: false
-//      };
-//    } else {
-//      data[countryCode][commonName].obs.push(parts);
-//      data[countryCode][commonName].numObs++;
-//    }
-//  }))
-//  .on('error', function() {
-//    console.log('Error while reading file.');
-//  })
-//  .on('end', function() {
-//
-//    // lastly, serialize the data and write to a file
-//    fs.writeFile("./results/result-1.txt", JSON.stringify(data), function(err) {
-//      if (err) {
-//        return console.log(err);
-//      }
-//      console.log("The file was saved.");
-//    });
-//  });
-
 
 
